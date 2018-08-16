d0da2fa6bb23e60edca019a1cd78d9469dab7cac
Andrew <jaknowles1997@gmail.com>
2018-08-16 08:59:31 -0700

    Version 1.0.0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/api/items');
 
@@ -10,11 +11,20 @@ app.use(express.json());
 
 const db = require('./config/keys.js').mongoURI;
 
-mongoose.connect(db)
+mongoose.connect(db, {useNewUrlParser: true})
     .then(() => console.log('MongoDB Connected!'))
     .catch(err => console.log(err));
 
 app.use('/api/items', items);
+
+// Serve statitic assets if in production
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (requestAnimationFrame, res) => {
+        res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    }); 
+}
 
 const port = process.env.PORT || 5000;
 
